d2562af4b40fe0435385233ba4a9f9cd7418e264
Babouchot <bastien.maureille@hotmail.fr>
2013-05-12 21:14:09 +0200

    reading from file

---
+++ grip.lua
@@ -0,0 +1,44 @@
+-- main loop of the programm
+require "loop"
+
+version =" 1.0"
+
+print("GNU RIP " .. version .. [[ 
+Welcome to GNU RIP, the GNU Recursive Interpretation Processor
+By Gwenn Aubert and Bastien Maureille -- 2013
+type help for details
+
+     °,
+      `'
+        \ ___
+         /   7
+        (_/_/\
+         \    \
+          \    \
+          _\    \__
+         (   \     )
+          \___\___/ 
+
+]])
+
+if (arg[1] ~= nil) then
+    file = io.open(arg[1])
+    reading = true
+    count = 1
+    while (reading) do
+        line = file:read()
+        if (line ~= nil) then
+            print(count.." "..line)
+            main_loop(line)
+            count = count + 1
+        else
+            reading = false
+        end
+    end
+else
+    while true do
+        cmd = prompt()
+        print (cmd)
+        main_loop(cmd)
+    end
+end
